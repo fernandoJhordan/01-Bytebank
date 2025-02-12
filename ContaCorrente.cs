2691e3cdf0eb53de207b185b5f637807059c879f
Fernando Michael <nando_michael@hhotmail.com>
2025-02-12 17:45:41 -0300

    Primeiro Commit

---
+++ ContaCorrente.cs
@@ -0,0 +1,9 @@
+﻿
+
+class ContaCorrente 
+{ 
+    public string titular;
+    public int numeroAgencia;
+    public int numero;
+    public double saldo;
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.numeroAgencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo =  100;
+
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine("Agência: " + contaDaGabriela.numeroAgencia);
+            Console.WriteLine("Número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+
+            Console.ReadLine();
+
+     
+        }
+    }
+}
